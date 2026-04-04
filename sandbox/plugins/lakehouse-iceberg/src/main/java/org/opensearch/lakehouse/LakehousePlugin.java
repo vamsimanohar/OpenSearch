f2d263a9768b7d275640cf3061ed6d85d1ca0a13
Vamsi Manohar <reddyvam@amazon.com>
2026-04-04 22:45:28 +0000

    feat(lakehouse): REST API for catalog and table registration with cluster state persistence
    Signed-off-by: Vamsi Manohar <reddyvam@amazon.com>

--- sandbox/plugins/lakehouse-iceberg/src/main/java/org/opensearch/lakehouse/LakehousePlugin.java
+++ sandbox/plugins/lakehouse-iceberg/src/main/java/org/opensearch/lakehouse/LakehousePlugin.java
@@ -8,8 +8,100 @@
 
 package org.opensearch.lakehouse;
 
+import org.opensearch.cluster.metadata.IndexNameExpressionResolver;
+import org.opensearch.cluster.metadata.Metadata;
+import org.opensearch.cluster.node.DiscoveryNodes;
+import org.opensearch.cluster.service.ClusterService;
+import org.opensearch.common.settings.ClusterSettings;
+import org.opensearch.common.settings.IndexScopedSettings;
+import org.opensearch.common.settings.Settings;
+import org.opensearch.common.settings.SettingsFilter;
+import org.opensearch.core.common.io.stream.NamedWriteableRegistry;
+import org.opensearch.core.xcontent.NamedXContentRegistry;
+import org.opensearch.env.Environment;
+import org.opensearch.env.NodeEnvironment;
+import org.opensearch.lakehouse.action.RegisterCatalogAction;
+import org.opensearch.lakehouse.action.RegisterTableAction;
+import org.opensearch.lakehouse.cluster.LakehouseMetadata;
+import org.opensearch.plugins.ActionPlugin;
 import org.opensearch.plugins.Plugin;
+import org.opensearch.repositories.RepositoriesService;
+import org.opensearch.rest.RestController;
+import org.opensearch.rest.RestHandler;
+import org.opensearch.script.ScriptService;
+import org.opensearch.threadpool.ThreadPool;
+import org.opensearch.transport.client.Client;
+import org.opensearch.watcher.ResourceWatcherService;
 
-public class LakehousePlugin extends Plugin {
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Supplier;
+
+/**
+ * Plugin for reading external Apache Iceberg tables via PPL and SQL.
+ * Provides REST APIs for registering catalogs and tables, with cluster state persistence.
+ */
+public class LakehousePlugin extends Plugin implements ActionPlugin {
+
+    private ClusterService clusterService;
+
     public LakehousePlugin() {}
+
+    @Override
+    public Collection<Object> createComponents(
+        Client client,
+        ClusterService clusterService,
+        ThreadPool threadPool,
+        ResourceWatcherService resourceWatcherService,
+        ScriptService scriptService,
+        NamedXContentRegistry xContentRegistry,
+        Environment environment,
+        NodeEnvironment nodeEnvironment,
+        NamedWriteableRegistry namedWriteableRegistry,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<RepositoriesService> repositoriesServiceSupplier
+    ) {
+        this.clusterService = clusterService;
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<RestHandler> getRestHandlers(
+        Settings settings,
+        RestController restController,
+        ClusterSettings clusterSettings,
+        IndexScopedSettings indexScopedSettings,
+        SettingsFilter settingsFilter,
+        IndexNameExpressionResolver indexNameExpressionResolver,
+        Supplier<DiscoveryNodes> nodesInCluster
+    ) {
+        return List.of(
+            new RegisterCatalogAction(clusterService),
+            new RegisterTableAction(clusterService)
+        );
+    }
+
+    @Override
+    public List<NamedWriteableRegistry.Entry> getNamedWriteables() {
+        return List.of(
+            new NamedWriteableRegistry.Entry(Metadata.Custom.class, LakehouseMetadata.TYPE, LakehouseMetadata::new),
+            new NamedWriteableRegistry.Entry(
+                org.opensearch.cluster.NamedDiff.class,
+                LakehouseMetadata.TYPE,
+                LakehouseMetadata::readDiffFrom
+            )
+        );
+    }
+
+    @Override
+    public List<NamedXContentRegistry.Entry> getNamedXContent() {
+        return List.of(
+            new NamedXContentRegistry.Entry(
+                Metadata.Custom.class,
+                new org.opensearch.core.ParseField(LakehouseMetadata.TYPE),
+                parser -> LakehouseMetadata.fromXContent(parser)
+            )
+        );
+    }
 }
